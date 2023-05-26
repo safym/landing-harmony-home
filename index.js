1b6ae726a333f8edd2f7c35ff5d769157f5d50a4
safym <osafym@gmail.com>
2023-05-26 22:16:47 +0300

    add scroll to top

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { PRODUCT_ID_SELECTOR, addDateToItems, setData } from './src/scripts/index.js'
 
+// Добавить дату на карточки товаров
 addDateToItems(PRODUCT_ID_SELECTOR)
 
+// Обработчик кликов по документу и форме
 const handleClick = (event) => {
   const dialog = document.getElementById('modal')
+  const form = dialog.querySelector('form')
   const productElement = event.target.closest(PRODUCT_ID_SELECTOR)
 
   const isBuyButton = event.target.dataset?.addbutton
@@ -13,14 +16,17 @@ const handleClick = (event) => {
     dialog.showModal()
   } else {
     if (event.target.id === 'modalCancel') {
+      form.reset()
       dialog.close()
     } else if (event.target.id === 'modalConfirm') {
-      alert('Confirmed!')
+      form.submit()
       dialog.close()
+      alert('Confirmed!')
     }
   }
 }
 
+// Функция для изменения темы
 const changeTheme = () => {
   const element = document.body
   const isDarkTheme = eval(element.dataset.dark)
@@ -28,8 +34,27 @@ const changeTheme = () => {
   element.dataset.dark = !isDarkTheme
 }
 
+// Функция для переключения видимости элемента который скроллит страницу вверх
+const toggleScrollToTop = () => {
+  const scrollToTop = document.querySelector('.scrollToTop');
+  // console.log('scroll')
+  if (window.pageYOffset > 100) {
+    scrollToTop.classList.add('scrollToTop-show');
+  } else {
+    scrollToTop.classList.remove('scrollToTop-show');
+  }
+}
+
+// Функция скролла страницы вверх
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Инициирование eventListeners
 window.document.addEventListener('click', handleClick)
 
-const themeToggler = document.getElementById('themeToggler')
+document.getElementById('themeToggler').addEventListener('click', changeTheme)
 
-themeToggler.addEventListener('click', changeTheme)
+window.addEventListener('scroll', toggleScrollToTop);
+
+document.querySelector('.scrollToTop').addEventListener('click', scrollToTop);
